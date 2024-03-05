173315ddefa5ecd0f4ccb4ef6b8100b43e95b8fc
Jacqueline Oliveira <jacqueline.alterdata@gmail.com>
2024-03-05 14:18:03 -0300

    vídeo 2.3

--- src/test/java/br/com/alura/codechella/domain/entities/usuario/UsuarioTest.java
+++ src/test/java/br/com/alura/codechella/domain/entities/usuario/UsuarioTest.java
@@ -19,4 +19,18 @@ public class UsuarioTest {
                 () -> new Usuario("", "Jacque", LocalDate.parse("1990-09-08"), "email@email.com"));
 
     }
+
+    @Test
+    public void deveCriarUsuarioUsandoFabricaDeUsuario(){
+        FabricaDeUsuario fabrica = new FabricaDeUsuario();
+        Usuario usuario = fabrica.comNomeCpfNascimento("Emily", "654.123.897-88",
+                LocalDate.parse("2000-10-01"));
+
+        Assertions.assertEquals("Emily", usuario.getNome());
+
+        usuario = fabrica.incluiEndereco("12345-999", 40, "apto 201");
+
+        Assertions.assertEquals("apto 201", usuario.getEndereco().getComplemento());
+
+    }
 }
