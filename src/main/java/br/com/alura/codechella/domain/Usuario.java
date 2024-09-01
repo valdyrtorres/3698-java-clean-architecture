66dbfbf4974d5bf4a9c76d19a7a82ff4b3ed4472
Valdir Github <valdyrtorres@yahoo.com.br>
2024-09-01 11:48:00 -0300

    inicio refatoracao, criacao classe usuario na clean architecture

--- src/main/java/br/com/alura/codechella/domain/Usuario.java
+++ src/main/java/br/com/alura/codechella/domain/Usuario.java
@@ -1,31 +1,12 @@
-package br.com.alura.codechella.model;
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+package br.com.alura.codechella.domain;
 
 import java.time.LocalDate;
 
-@Entity
-@Table(name = "usuarios")
 public class Usuario {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
     private String cpf;
     private String nome;
     private LocalDate nascimento;
     private String email;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 
     public String getCpf() {
         return cpf;
--- src/main/java/br/com/alura/codechella/naousar/repository/UsuarioRepository.java
+++ src/main/java/br/com/alura/codechella/naousar/repository/UsuarioRepository.java
@@ -1,6 +1,6 @@
-package br.com.alura.codechella.repository;
+package br.com.alura.codechella.naousar.repository;
 
-import br.com.alura.codechella.model.Usuario;
+import br.com.alura.codechella.naousar.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
