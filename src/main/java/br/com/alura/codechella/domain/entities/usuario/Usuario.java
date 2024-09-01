98b22da6cabc19e1cb6c70556cba56eaf19d8f46
Valdir Github <valdyrtorres@yahoo.com.br>
2024-09-01 12:28:50 -0300

    criacao fabrica de usuario aula2

--- src/main/java/br/com/alura/codechella/domain/entities/usuario/Usuario.java
+++ src/main/java/br/com/alura/codechella/domain/entities/usuario/Usuario.java
@@ -1,4 +1,6 @@
-package br.com.alura.codechella.domain;
+package br.com.alura.codechella.domain.entities.usuario;
+
+import br.com.alura.codechella.domain.Endereco;
 
 import java.time.LocalDate;
 
@@ -7,6 +9,28 @@ public class Usuario {
     private String nome;
     private LocalDate nascimento;
     private String email;
+
+    private Endereco endereco;
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+    public Usuario(String cpf, String nome, LocalDate nascimento, String email) {
+
+        if (cpf == null || !cpf.matches("\\d{3}\\.\\d{3}\\.\\d{3}\\-\\d{2}")) {
+            throw new IllegalArgumentException("Cpf no padrão incorreto!");
+        }
+
+        this.cpf = cpf;
+        this.nome = nome;
+        this.nascimento = nascimento;
+        this.email = email;
+    }
 
     public String getCpf() {
         return cpf;
---
+++ src/test/java/br/com/alura/codechella/domain/entities/usuario/UsuarioTest.java
@@ -0,0 +1,14 @@
+package br.com.alura.codechella.domain.entities.usuario;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+public class UsuarioTest {
+    @Test
+    public void naoDeveCadastrarUsuarioComCpfNoFormatoInvalido() {
+        Assertions.assertThrows(IllegalArgumentException.class,
+                () -> new Usuario("123456789-99", "Val", LocalDate.parse("1969-06-11"), "email@email.com"));
+    }
+}
